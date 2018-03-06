cfdd6a4f3a3d4eead751a03e79d160a07370b16e
hyunsik <shs9589@gmail.com>
2018-03-06 17:10:24 +0900

    add loading state

--- src/App.js
+++ src/App.js
@@ -2,43 +2,52 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-    {
-    title : "Matrix",
-    poster : "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYxMjcwNDIzMF5BMl5BanBnXkFtZTgwNzUxNjU0NzE@._V1_.jpg"
-  },
-  {
-    title : "Full Metal Jacket",
-    poster : "https://www.tynesidecinema.co.uk/storage/app/uploads/public/59c/555/720/59c5557200731645091417.jpg"
-  },
-  {
-    title :  "Old Boy",
-    poster : "https://a.ltrbxd.com/resized/sm/upload/c9/3j/2v/r4/oldboy-1200-1200-675-675-crop-000000.jpg?k=a1b8014ebc"
-  },
-  {
-    title : "Star Wars" ,
-    poster : "https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/630/354/75/wdpromedia.disney.go.com/media/wdpro-assets/parks-and-tickets/tours-and-experiences/star-wars-guided-tour/star-wars-guided-tour-00.jpg?21102016062907"
-  }
-]
-
 class App extends Component {
 
-  componentWillMount(){
-    console.log('willmount');
+  state= {
 
   }
 
   componentDidMount(){
-    console.log('didmount');
+    setTimeout(() => {
+      this.setState({
+        movies : [
+          {
+            title : "Matrix",
+            poster : "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYxMjcwNDIzMF5BMl5BanBnXkFtZTgwNzUxNjU0NzE@._V1_.jpg"
+          },
+          {
+            title : "Full Metal Jacket",
+            poster : "https://www.tynesidecinema.co.uk/storage/app/uploads/public/59c/555/720/59c5557200731645091417.jpg"
+          },
+          {
+            title :  "Old Boy",
+            poster : "https://a.ltrbxd.com/resized/sm/upload/c9/3j/2v/r4/oldboy-1200-1200-675-675-crop-000000.jpg?k=a1b8014ebc"
+          },
+          {
+            title : "Star Wars" ,
+            poster : "https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/630/354/75/wdpromedia.disney.go.com/media/wdpro-assets/parks-and-tickets/tours-and-experiences/star-wars-guided-tour/star-wars-guided-tour-00.jpg?21102016062907"
+          }, 
+          {
+            title : "HarryPotter",
+            poster : "https://nerdist.com/wp-content/uploads/2015/10/harry-potter-image.jpg"
+          }
+        ]
+      })
+    },5000)
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title = {movie.title} poster = {movie.poster} key = {index}/>
+    })
+    return movies
   }
 
   render() {
-    console.log('render');
     return (
       <div className="App">
-        {movies.map((movie, index) => {
-          return <Movie title = {movie.title} poster = {movie.poster} key = {index}/>
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading...'}
       </div>    
     );
   }
