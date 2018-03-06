a21fbbdb297f65e31e607bea4d96b782082c4dc8
hyunsik <shs9589@gmail.com>
2018-03-07 00:10:56 +0900

    add ajax
    with async and await

--- src/App.js
+++ src/App.js
@@ -4,44 +4,31 @@ import Movie from './Movie';
 
 class App extends Component {
 
-  state= {
-
-  }
+  state = {}
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies : [
-          {
-            title : "Matrix",
-            poster : "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYxMjcwNDIzMF5BMl5BanBnXkFtZTgwNzUxNjU0NzE@._V1_.jpg"
-          },
-          {
-            title : "Full Metal Jacket",
-            poster : "https://www.tynesidecinema.co.uk/storage/app/uploads/public/59c/555/720/59c5557200731645091417.jpg"
-          },
-          {
-            title :  "Old Boy",
-            poster : "https://a.ltrbxd.com/resized/sm/upload/c9/3j/2v/r4/oldboy-1200-1200-675-675-crop-000000.jpg?k=a1b8014ebc"
-          },
-          {
-            title : "Star Wars" ,
-            poster : "https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/630/354/75/wdpromedia.disney.go.com/media/wdpro-assets/parks-and-tickets/tours-and-experiences/star-wars-guided-tour/star-wars-guided-tour-00.jpg?21102016062907"
-          }, 
-          {
-            title : "HarryPotter",
-            poster : "https://nerdist.com/wp-content/uploads/2015/10/harry-potter-image.jpg"
-          }
-        ]
-      })
-    },5000)
+    this._getMovies()
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title = {movie.title} poster = {movie.poster} key = {index}/>
+    const movies = this.state.movies.map(movie => {
+      return <Movie title = {movie.title} poster = {movie.large_cover_image} key = {movie.id}/>
     })
     return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render() {
